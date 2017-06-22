d66b6d25b29aa69c05b61a396a3483862de3da81
cale <cale.bradbury@gmail.com>
2017-06-22 15:52:58 -0400

    replaced refrences to foo with level

--- PrefabDropdown/Assets/Arcturus/Attributes/Editor/PrefabDropdownDrawer.cs
+++ PrefabDropdown/Assets/Arcturus/Attributes/Editor/PrefabDropdownDrawer.cs
@@ -1,10 +1,6 @@
-﻿
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using System;
-using System.Reflection;
 
 [CustomPropertyDrawer(typeof(PrefabDropdownAttribute))]
 public class PrefabDropdownDrawer : PropertyDrawer
---
+++ PrefabDropdown/Assets/Level.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    [PrefabDropdown("Subfolder")]
+    public Level levelToLoad;
+    
+}
